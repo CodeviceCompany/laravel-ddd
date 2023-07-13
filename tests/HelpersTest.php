fe040fa1868a77df6671cc0307ff6ec280b300b6
Amine TIYAL <aminetiyal@gmail.com>
2023-07-13 10:34:02 +0100

    WIP

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -1,5 +1,5 @@
 <?php
 
 test('src_path is registered', function () {
-    expect(src_path())->toBe(app()->basePath('/src'));
+    expect(src_path())->toBe(app()->basePath() . DIRECTORY_SEPARATOR . 'src');
 });
