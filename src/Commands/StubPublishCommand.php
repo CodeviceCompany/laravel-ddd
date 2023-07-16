2824a7259e3f06f7b8b32213fd2b44723c8889b1
aminetiyal <aminetiyal@users.noreply.github.com>
2023-07-16 18:40:15 +0000

    Fix styling

--- src/Commands/StubPublishCommand.php
+++ src/Commands/StubPublishCommand.php
@@ -10,24 +10,24 @@ class StubPublishCommand extends BaseCommand
 {
     public function handle()
     {
-        if (!is_dir($stubsPath = $this->laravel->basePath('stubs'))) {
+        if (! is_dir($stubsPath = $this->laravel->basePath('stubs'))) {
             (new Filesystem)->makeDirectory($stubsPath);
         }
 
         $stubs = [
-            __DIR__ . '/stubs/action.data.php' => 'action.data.php',
-            __DIR__ . '/stubs/action.stub' => 'action.stub',
-            __DIR__ . '/stubs/data.stub' => 'data.stub',
-            __DIR__ . '/stubs/view-model.stub' => 'view-model.stub',
-            __DIR__ . '/stubs/model.stub' => 'model.stub',
+            __DIR__.'/stubs/action.data.php' => 'action.data.php',
+            __DIR__.'/stubs/action.stub' => 'action.stub',
+            __DIR__.'/stubs/data.stub' => 'data.stub',
+            __DIR__.'/stubs/view-model.stub' => 'view-model.stub',
+            __DIR__.'/stubs/model.stub' => 'model.stub',
         ];
 
         $this->laravel['events']->dispatch($event = new PublishingStubs($stubs));
 
         foreach ($event->stubs as $from => $to) {
-            $to = $stubsPath . DIRECTORY_SEPARATOR . ltrim($to, DIRECTORY_SEPARATOR);
+            $to = $stubsPath.DIRECTORY_SEPARATOR.ltrim($to, DIRECTORY_SEPARATOR);
 
-            if ((!$this->option('existing') && (!file_exists($to) || $this->option('force')))
+            if ((! $this->option('existing') && (! file_exists($to) || $this->option('force')))
                 || ($this->option('existing') && file_exists($to))) {
                 file_put_contents($to, file_get_contents($from));
             }
